29024aecba7bf2b73891461e841ca506c3831fdc
Lang-Shin <bajuracharles@gmail.com>
2026-04-17 16:28:47 +0800

    Population Growth Analysis Initial Complete

--- Population-Growth-Analysis/pga.py
+++ Population-Growth-Analysis/pga.py
@@ -28,7 +28,6 @@ def total_change(df):
     )
 
 
-
 df = pd.read_csv("Population-Growth-Analysis/population_data.csv")
 
 grate2021 = growth_rate(df, 2021)
@@ -39,3 +38,22 @@ print(df)
 
 
 # VISUALIZATION
+fig, axes = plt.subplots(1, 2, figsize=(13, 6))
+axes[0].plot(df['year'], df['population'], marker='o', color="#2C7B0A")
+axes[0].set_xticks(df['year'])
+axes[0].grid(True, alpha=0.6, linestyle='-', animated=True)
+axes[0].set_title("Population vs Time", fontweight="bold")
+axes[0].set_xlabel("Year", fontweight="bold")
+axes[0].set_ylabel("Population", fontweight="bold")
+
+years = np.array([2021, 2022, 2023])
+grates = np.array([grate2021, grate2022, grate2023])
+axes[1].plot(years, grates, marker='o', color="#2C7B0A")
+axes[1].set_xticks(years)
+axes[1].grid(True, alpha=0.6, linestyle='-')
+axes[1].set_title("Growth Rate vs Time", fontweight="bold")
+axes[1].set_xlabel("Year", fontweight="bold")
+axes[1].set_ylabel("Growth Rate", fontweight="bold")
+
+plt.tight_layout()
+plt.show()
