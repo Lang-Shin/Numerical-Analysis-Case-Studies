2afe88e7756f9281f075b889319dcb6a2d4f4256
Lang-Shin <bajuracharles@gmail.com>
2026-04-17 16:36:16 +0800

    Added the data for other 2

---
+++ Traffic-Flow-Velocity-Estimation/tfve.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv("Traffic-Flow-Velocity-Estimation/data.csv")
+
+print(df)
---
+++ Water-Tank-Filling-Rate-Analysis/wtfra.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+df = pd.read_csv("Water-Tank-Filling-Rate-Analysis/data.csv")
+
+print(df)
